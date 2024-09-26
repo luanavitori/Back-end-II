6707d9e19c13023d37d391f385100d910f3e0149
luanavitori <luanavha2007@gmail.com>
2024-09-26 11:45:32 -0400

    novo

---
+++ Api-Atividades/dadosAP.js
@@ -0,0 +1,48 @@
+const colecaoAP = [
+    {
+        id: 1,
+        cidade: 'Macapá'
+    },
+    {
+        id: 2,
+        cidade: 'Santana'
+    },
+    {
+        id: 3,
+        cidade: 'Laranjal do Jari'
+    },
+    {
+        id: 4,
+        cidade: 'Oiapoque'
+    },
+    {
+        id: 5,
+        cidade: 'Mazagão'
+    },
+    {
+        id: 6,
+        cidade: 'Porto Grande'
+    },
+    {
+        id: 7,
+        cidade: 'Tartarugalzinho'
+    },
+    {
+        id: 8,
+        cidade: 'Pedra Branca do Amari'
+    },
+    {
+        id: 9,
+        cidade: 'Vitória do Jari'
+    },
+    {
+        id: 10,
+        cidade: 'Calçoene'
+    },
+    {
+        id: 11,
+        cidade: 'Amapá'
+    },
+    
+    
+]
