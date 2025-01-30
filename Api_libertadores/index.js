afe2af67c75c61fd216af8c6b3fdc26df2277162
luanavitori <luanavha2007@gmail.com>
2025-01-30 11:42:00 -0400

    aaa

---
+++ Api_libertadores/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+/*import pool from './servico/conexao.js';*/
+import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import {retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+
+const app = express();
+
+app.get('/campeonatos', async (req,res) => {
+   const campeonatos = await retornaCampeonatos();
+   res.json(campeonatos);
+})
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = perseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    res.json(campeonato);
+});
+
+
+app.listen(9000, async () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: "+data);
+
+    /*const conexao = await pool.getConnection();
+
+    console.log(conexao.threadId);
+
+    conexao.release();*/
+})
+
---
+++ Api_libertadores/servico/retornaCampeonatos_servico.js
@@ -0,0 +1,17 @@
+import pool from './conexao.js';
+
+export async function retornaCampeonatos() {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos');
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosID(id) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id = '+id);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
